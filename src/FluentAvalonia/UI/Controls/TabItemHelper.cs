054228cfc9488483b3075b8959de643009311552
RealMeddsam <realferidjelalia@gmail.com>
2026-09-09 21:01:44 +0100

    Update Tab Control Style

---
+++ src/FluentAvalonia/UI/Controls/TabItemHelper.cs
@@ -0,0 +1,15 @@
+﻿#nullable enable
+using Avalonia;
+using Avalonia.Controls;
+
+namespace FluentAvalonia.UI.Controls;
+
+public class TabItemHelper
+{
+    public static readonly AttachedProperty<object?> IconProperty =
+        AvaloniaProperty.RegisterAttached<TabItem, object?>("Icon", typeof(TabItemHelper));
+
+    public static object? GetIcon(TabItem element) => element.GetValue(IconProperty);
+
+    public static void SetIcon(TabItem element, object? value) => element.SetValue(IconProperty, value);
+}
